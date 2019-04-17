f39799c5da41ae6e8571b5e3d2d1d4e8d1e54789
Herve <nenes_fr@yahoo.fr>
2019-04-17 14:04:48 +0200

    WIP modules

--- src/Hhennes/PrestashopConsole/Command/Module/Generate/ControllerCommand.php
+++ src/Hhennes/PrestashopConsole/Command/Module/Generate/ControllerCommand.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2018 Hennes Hervé
+ * 2007-2019 Hennes Hervé
  *
  * NOTICE OF LICENSE
  *
@@ -13,7 +13,7 @@
  * to contact@h-hennes.fr so we can send you a copy immediately.
  *
  * @author    Hennes Hervé <contact@h-hennes.fr>
- * @copyright 2007-2018 Hennes Hervé
+ * @copyright 2007-2019 Hennes Hervé
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * http://www.h-hennes.fr/blog/
  */
@@ -21,11 +21,8 @@
 namespace Hhennes\PrestashopConsole\Command\Module\Generate;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
--- src/Hhennes/PrestashopConsole/Command/Module/Generate/UpgradeCommand.php
+++ src/Hhennes/PrestashopConsole/Command/Module/Generate/UpgradeCommand.php
@@ -1,17 +1,21 @@
 <?php
 /**
- * 2002-2019 ADVISA
+ * 2007-2019 Hennes Hervé
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0) that is available
- * through the world-wide-web at this URL: http://www.opensource.org/licenses/OSL-3.0
- * If you are unable to obtain it through the world-wide-web, please send an email
- * to mage@advisa.fr so we can send you a copy immediately.
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@h-hennes.fr so we can send you a copy immediately.
  *
- * @author ADVISA
- * @copyright 2002-2019 ADVISA
- * @license http://www.opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @author    Hennes Hervé <contact@h-hennes.fr>
+ * @copyright 2007-2019 Hennes Hervé
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * http://www.h-hennes.fr/blog/
  */
 
 namespace Hhennes\PrestashopConsole\Command\Module\Generate;
@@ -66,7 +70,7 @@ class UpgradeCommand extends Command
         try {
             $this->_createDirectories();
         } catch (IOException $e) {
-            $output->writeln('<error>Unable to creat ugrade directory</error>');
+            $output->writeln('<error>Unable to create upgrade directory</error>');
             return false;
         }
 
@@ -79,7 +83,7 @@ class UpgradeCommand extends Command
                 $defaultContent
             );
         } catch (IOException $e) {
-            $output->writeln('<error>Unable to creat upgrade file</error>');
+            $output->writeln('<error>Unable to create upgrade file</error>');
             return false;
         }
 
